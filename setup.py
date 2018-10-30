978cdb22507b866f71fe40f9a7969161ce05d942
Fernando Stecconi <nandilugio@gmail.com>
2018-10-30 15:47:54 +0100

    Bumping version: 0.1.0 → 0.2.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
     name="pepython",
-    version="0.1.0",
+    version="0.2.0",
     description="Simple task automation for projects.",
     long_description=long_description,
     long_description_content_type="text/markdown",
