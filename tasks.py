f4a40b3cd0dfbb2779a6c51090b526836770e7a4
Fernando Stecconi <nandilugio@gmail.com>
2018-10-30 14:35:36 +0100

    Project tasks wrapup. Implemented interactive param for s()

--- tasks.py
+++ tasks.py
@@ -2,31 +2,25 @@ from pepython.task_def import task, s
 
 
 @task
-def clean(*args):
+def clean():
     s("rm -rf build dist *.egg-info")
+    s("pipenv install -e .")
 
 
 @task
-def build(*args):
-    s("pipenv run pip install --upgrade setuptools wheel")
+def build():
+    s("pipenv run pip install --upgrade setuptools wheel")  # We _really_ want last versions ;p
+    clean()
     s("pipenv run python setup.py sdist bdist_wheel")
 
 
 @task
-def publish(*args):
-    s("pipenv run pip install --upgrade twine")
-    #s("pipenv run twine upload --repository-url https://test.pypi.org/legacy/ dist/*")
-    s("pipenv run twine upload dist/*")
+def publish():
+    s("pipenv run twine upload --repository-url https://test.pypi.org/legacy/ dist/*", interactive=True)
+    #s("pipenv run twine upload dist/*", interactive=True)
 
 
 @task
 def build_and_publish(*args):
-    clean()
     build()
     publish()
-
-# TODO: delete!!
-@task
-def test(*args):
-    res = s("ls " + args[0])
-    print("res was:\n" + res.value['out'])
